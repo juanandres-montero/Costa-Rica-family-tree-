ad502a81711f311ef2b43589fc654936873915a8
juanandres-montero <juanandres.montero@outlook.com>
2024-03-27 17:17:17 -0600

    clear

--- afcr.js
+++
@@ -1,8 +0,0 @@
-const puppeteer = require('puppeteer-web');
-async function performWebSearch(cedulaNumber) {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 800 });
-    await page.goto('https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx');
-    
-}
